4f6855749eb16c0032268e51f2a55764de245d4f
Bu11.08 <ntkiet11082002@gmail.com>
2024-07-14 16:04:07 +0700

    chatbot

--- Backend/config/vnpayConfig.js
+++ Backend/config/vnpayConfig.js
@@ -3,5 +3,5 @@ module.exports = {
   vnp_TmnCode: "A14ZG7RU",
   vnp_HashSecret: "GWZVNUYKOESWJXIFUXGSWNQQLSJCPQGT",
   vnp_Url: "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-  vnp_ReturnUrl: "https://a2kshopfe.vercel.app//vnpay_return",
+  vnp_ReturnUrl: "https://a2kshopfe.vercel.app/vnpay_return",
 };
